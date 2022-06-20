a90a4bc06377496129fb73ac4446b76ae3b53ddb
rafaelatraldi <rafaelatraldic@gmail.com>
2022-06-20 17:59:21 -0300

    tela
    Adicionada a tela do jogo

--- src/main/Game.java
+++ src/main/Game.java
@@ -1,11 +1,13 @@
 package main;
 
 public class Game {
-	
 	private GameWindow gameWindow;
+	private GamePanel gamePanel;
+	
 	
 	public Game() {
+		gamePanel = new GamePanel();
+		gameWindow = new GameWindow(gamePanel);
 		
-		gameWindow = new GameWindow();
 	}
 }
--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -1,5 +1,18 @@
 package main;
 
-public class GamePanel {
+import java.awt.Graphics;
 
+import javax.swing.JPanel;
+
+public class GamePanel extends JPanel {
+	
+	public GamePanel() {
+		
+	}
+		
+	public void paintComponent(Graphics g) {
+		super.paintComponent(g);
+		
+		g.fillRect(100, 100, 200, 50);
+	}
 }
